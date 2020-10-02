79a687910e0914c802ce9228d9f2da2a84510ac1
vamsi-krish-chiguruwada <vamsikrishchiguruwada@gmail.com>
2020-10-01 21:12:22 -0700

    Updates

---
+++ precache-manifest.7af860c24657e94b48e0674db04d5236.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f92920df0a63e04c6c84cfb1fea4364",
+    "url": "/popup.io/index.html"
+  },
+  {
+    "revision": "e724b6d38c1a4ef108f7",
+    "url": "/popup.io/static/css/main.05198650.chunk.css"
+  },
+  {
+    "revision": "fbd0a0b95e2558bb88e1",
+    "url": "/popup.io/static/js/2.3bd58dd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/popup.io/static/js/2.3bd58dd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e724b6d38c1a4ef108f7",
+    "url": "/popup.io/static/js/main.e1d2a756.chunk.js"
+  },
+  {
+    "revision": "c5971cc657d1b89125dd",
+    "url": "/popup.io/static/js/runtime-main.9962f97f.js"
+  }
+]);
